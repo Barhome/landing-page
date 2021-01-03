e710fda615f59d47e2cc99af25b17b5deffa6f96
Barhome <ielsamoly@gmail.com>
2021-01-03 07:25:26 +0200

    feat:Add active state to the section when it appears in the viewport

--- js/app.js
+++ js/app.js
@@ -19,13 +19,36 @@
  */
 const navbarList = document.getElementById("navbar__list");
 const sectionName = document.querySelectorAll(".section-name");
-
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
+const section4 = document.getElementById("section4");
+console.log(section1.getBoundingClientRect());
+console.log(section2.getBoundingClientRect());
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
 
+// check if a section is in the viewport
+
+isInView = function (element) {
+  let boundries = element.getBoundingClientRect();
+  return (
+    boundries.top >= 0 &&
+    boundries.left >= 0 &&
+    boundries.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    boundries.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// isOutView = function (element) {
+//   let boundries = element.getBoundingClientRect();
+//   if (boundries.bottom === 0) return true;
+// };
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -44,6 +67,40 @@ window.addEventListener("load", function () {
 });
 
 // Add class 'active' to section when near top of viewport
+document.addEventListener("scroll", function () {
+  if (isInView(section1)) {
+    setTimeout(function () {
+      section1.classList.add("your-active-class");
+      section2.classList.remove("your-active-class");
+      section3.classList.remove("your-active-class");
+      section4.classList.remove("your-active-class");
+    }, 500);
+  }
+  if (isInView(section2)) {
+    setTimeout(function () {
+      section1.classList.remove("your-active-class");
+      section2.classList.add("your-active-class");
+      section3.classList.remove("your-active-class");
+      section4.classList.remove("your-active-class");
+    }, 500);
+  }
+  if (isInView(section3)) {
+    setTimeout(function () {
+      section1.classList.remove("your-active-class");
+      section2.classList.remove("your-active-class");
+      section3.classList.add("your-active-class");
+      section4.classList.remove("your-active-class");
+    }, 500);
+  }
+  if (isInView(section4)) {
+    setTimeout(function () {
+      section1.classList.remove("your-active-class");
+      section2.classList.remove("your-active-class");
+      section3.classList.remove("your-active-class");
+      section4.classList.add("your-active-class");
+    }, 500);
+  }
+});
 
 // Scroll to anchor ID using scrollTO event
 
